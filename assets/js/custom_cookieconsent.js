f22b1e1ba68946a3005e569648a5a30a3cffbcb2
Joel Strasser <strasser999@gmail.com>
2020-07-29 23:53:03 +0200

    add cookie consent

---
+++ assets/js/custom_cookieconsent.js
@@ -0,0 +1,15 @@
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#edeff5",
+      "text": "#838391"
+    },
+    "button": {
+      "background": "#4b81e8"
+    }
+  },
+  "theme": "classic",
+  "content": {
+    "href": "https://www.joestr.at/legal/index.html"
+  }
+});
